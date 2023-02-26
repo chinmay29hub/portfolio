73a6eba30aa70297382231be58997cd502921edf
chinmay29hub <chinmaysonawane57@gmail.com>
2023-02-26 15:59:46 +0530

    animation

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
   return (
 
     <AnimatePresence initial={ false }>
+    <Model />
 
     <div className="flex w-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20">
     
@@ -117,7 +118,7 @@ function App() {
       {/* <div className="container w-full mt-7"> */}
           {/* <Spline scene="https://prod.spline.design/HzsgUGRONN0p-MxS/scene.splinecode" /> */}
           <div className="w-full h-full">
-          <Model />
+          
 
           </div>
             

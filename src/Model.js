eb14a173d2868ef4475c7230f5301a5f49ea569f
chinmay29hub <chinmaysonawane57@gmail.com>
2023-03-15 20:20:27 +0530

    spline sucks

--- src/Model.js
+++ src/Model.js
@@ -4,10 +4,10 @@ export default function App() {
   return (
     <div style={{ height : "100vh" }}>
       {/* <Spline scene="https://prod.spline.design/2PPIdBzs0CXumg2p/scene.splinecode" /> */}
-      <Spline scene="https://prod.spline.design/hE8rMkc8o1y7QJ1U/scene.splinecode" />
+      {/* <Spline scene="https://prod.spline.design/hE8rMkc8o1y7QJ1U/scene.splinecode" /> */}
       {/* <Spline scene="https://prod.spline.design/HzsgUGRONN0p-MxS/scene.splinecode" /> */}
       {/* <Spline scene="https://prod.spline.design/O5BxeN05wvUbSTeq/scene.splinecode" /> */}
-      {/* <Spline scene="https://prod.spline.design/O5BxeN05wvUbSTeq/scene.splinecode" /> */}
+      <Spline scene="https://prod.spline.design/O5BxeN05wvUbSTeq/scene.splinecode" />
     </div>
   );
 }

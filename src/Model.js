5c173fd8d6965378e6df525f941d3a4fd7fc0aba
chinmay29hub <chinmaysonawane57@gmail.com>
2023-03-15 20:34:40 +0530

    spline sucks

--- src/Model.js
+++ src/Model.js
@@ -2,12 +2,12 @@ import Spline from "@splinetool/react-spline";
 
 export default function App() {
   return (
-    <div style={{ height : "100%", position : "relative", top: "90px" }}>
+    <div style={{ height : "80vh", position : "relative", top: "73px" }}>
       {/* <Spline scene="https://prod.spline.design/2PPIdBzs0CXumg2p/scene.splinecode" /> */}
       {/* <Spline scene="https://prod.spline.design/hE8rMkc8o1y7QJ1U/scene.splinecode" /> */}
       {/* <Spline scene="https://prod.spline.design/HzsgUGRONN0p-MxS/scene.splinecode" /> */}
       {/* <Spline scene="https://prod.spline.design/O5BxeN05wvUbSTeq/scene.splinecode" /> */}
-      <Spline style={{ height : "80vh" }} scene="https://prod.spline.design/O5BxeN05wvUbSTeq/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/O5BxeN05wvUbSTeq/scene.splinecode" />
     </div>
   );
 }

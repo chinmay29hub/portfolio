c393424065c0ac1c66bb2e7a67cb0d7c658ce7ef
chinmay29hub <chinmaysonawane57@gmail.com>
2023-03-23 23:18:38 +0530

    fix 3d design

--- src/Model.js
+++ src/Model.js
@@ -3,7 +3,7 @@ import Spline from "@splinetool/react-spline";
 export default function App() {
   const design = process.env.REACT_APP_SPLINE_DESIGN
   return (
-    <div style={{ height : "90vh", position : "relative", top: "73px" }}>
+    <div style={{ height : "90vh", position : "relative", top: "40px" }}>
       <Spline scene={ design } />
     </div>
   );
